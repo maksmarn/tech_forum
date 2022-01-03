48b18a3dfcfec5970b546a09e283c979d63f6028
maksmarn <maximarn@gmail.com>
2022-01-03 16:49:52 +0100

    some more updates

--- models/settings.py
+++ models/settings.py
@@ -1,5 +1,5 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db_url = os.getenv("DATABASE_URL", "sqlite:///loclahost.sqlite").replace("postgres://", "postgresql://", 1)
+db_url = os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite").replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(db_url)
