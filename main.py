6f5827740d254bf19548376a90dbfe1f3d820edc
maksmarn <maximarn@gmail.com>
2022-01-03 15:49:44 +0100

    another update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 # Handler 1
 @app.route('/', methods=["GET", "POST"])
-def hello_world():
+def index():
     if request.method == "GET":
         return render_template("index.html")
     
@@ -14,9 +14,24 @@ def hello_world():
 
 
 # Handler 2
-@app.route("/second-handler", methods=["GET"])
-def second_handler():
-    return "Here's the second handler"
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return "Here's the second handler"
+    
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        repeat = request.form.get("repeat")
+        
+        if password != repeat:
+            return "The two entered passwords don't match. Please try again."
+        
+        print(username)
+        print(password)
+        print(repeat)
+        
+        return redirect(url_for('index'))
 
 
 # Handler 3
