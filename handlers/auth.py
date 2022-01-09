ace3d9dd3cef7bd97ef5859d3c0db85376cefb38
maksmarn <maximarn@gmail.com>
2022-01-09 17:48:45 +0100

    auth handlers

---
+++ handlers/auth.py
@@ -0,0 +1,74 @@
+import hashlib
+import uuid
+from flask import make_response, redirect, url_for, request, render_template, Blueprint
+from models.settings import db
+from models.user import User
+
+auth_handlers = Blueprint("auth", __name__)
+
+
+@auth_handlers.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        # Get password hash out of password
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        
+        # Get user from the db by his/her username and password
+        user = db.query(User).filter_by(username=username).first()
+        
+        if not user:
+            return "This user doesn't exist!"
+        
+        else:
+            # If the user exists, check if the password hashes match
+            if password_hash == user.password_hash:
+                user.session_token = str(uuid.uuid4())
+                db.add(user)
+                db.commit()
+                
+                # Save the user's session token into a cookie
+                response = make_response(redirect(url_for('index')))
+                response.set_cookie("session_token", user.session_token, httponly=True, samesite='Strict')
+                
+                return response
+            else:
+                return "The password you entered is incorrect!"
+            
+            
+@auth_handlers.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        repeat = request.form.get("repeat")
+        
+        if password != repeat:
+            return "The two entered passwords don't match. Please try again."
+        
+        user = User(username=username,
+                    password_hash=hashlib.sha256(password.encode()).hexdigest(), 
+                    session_token=str(uuid.uuid4()))
+        db.add(user)  # Add to the transaction (user is not yet in the database)
+        db.commit()  # Commit the transaction into the database
+        
+        # Save the user's session token into a cookie
+        # HttpOnly and SameSite settings in a cookie provide greater security
+        # against attacks. httponly=True means that cookie cannot be accessed via
+        # Javascript. samesite='Strict' demands that that the cookie sender and
+        # receiver are on the same site. Thirdly you can also add secure=True, which
+        # would mean cookies can only be sent via HTTPS. But beware that this would
+        # mean cookies would not work on localhost, because your localhost uses HTTP.
+        response = make_response(redirect(url_for("index")))
+        response.set_cookie("session_token", user.session_token, httponly=True,
+                            samesite='Strict')
+        
+        return response
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import hashlib
 import uuid
 import os
 import smartninja_redis
@@ -6,10 +5,12 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from models.user import User
 from models.settings import db
 from models.topic import Topic
+from handlers.auth import auth_handlers
 
 redis = smartninja_redis.from_url(os.environ.get("REDIS_URL"))
 
 app = Flask(__name__)
+app.register_blueprint(auth_handlers)
 
 db.create_all()
 
@@ -27,73 +28,6 @@ def index():
     topics = db.query(Topic).all()
     
     return render_template("index.html", user=user, topics=topics)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    
-    elif request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        
-        # Get password hash out of password
-        password_hash = hashlib.sha256(password.encode()).hexdigest()
-        
-        # Get user from the db by his/her username and password
-        user = db.query(User).filter_by(username=username).first()
-        
-        if not user:
-            return "This user doesn't exist!"
-        
-        else:
-            # If the user exists, check if the password hashes match
-            if password_hash == user.password_hash:
-                user.session_token = str(uuid.uuid4())
-                db.add(user)
-                db.commit()
-                
-                # Save the user's session token into a cookie
-                response = make_response(redirect(url_for('index')))
-                response.set_cookie("session_token", user.session_token, httponly=True, samesite='Strict')
-                
-                return response
-            else:
-                return "The password you entered is incorrect!"
-
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "GET":
-        return render_template("signup.html")
-    
-    elif request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        repeat = request.form.get("repeat")
-        
-        if password != repeat:
-            return "The two entered passwords don't match. Please try again."
-        
-        user = User(username=username,
-                    password_hash=hashlib.sha256(password.encode()).hexdigest(), 
-                    session_token=str(uuid.uuid4()))
-        db.add(user)  # Add to the transaction (user is not yet in the database)
-        db.commit()  # Commit the transaction into the database
-        
-        # Save the user's session token into a cookie
-        # HttpOnly and SameSite settings in a cookie provide greater security
-        # against attacks. httponly=True means that cookie cannot be accessed via
-        # Javascript. samesite='Strict' demands that that the cookie sender and
-        # receiver are on the same site. Thirdly you can also add secure=True, which
-        # would mean cookies can only be sent via HTTPS. But beware that this would
-        # mean cookies would not work on localhost, because your localhost uses HTTP.
-        response = make_response(redirect(url_for("index")))
-        response.set_cookie("session_token", user.session_token, httponly=True,
-                            samesite='Strict')
-        
-        return response
 
 
 @app.route("/topic-create", methods=["GET", "POST"])
